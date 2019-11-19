8044b921b66ca3c5fa1832b43a75369bf66681cd
Evan Vogts <evanjvogts@gmail.com>
2019-11-19 11:59:47 -0600

    added submit new movie form functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,50 +1,116 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
-import LandingPage from '../../Routes/LandingPage/LandingPage';
-import LoginPage from '../../Routes/LoginPage/LoginPage';
-import NewUserPage from '../../Routes/NewUserPage/NewUserPage';
-import HomePage from '../../Routes/HomePage/HomePage';
-import ExpandedMoviePage from '../../Routes/ExpandedMoviePage/ExpandedMoviePage';
-import NewMoviePage from '../../Routes/NewMoviePage/NewMoviePage';
+import LandingPage from '../../routes/LandingPage/LandingPage';
+import LoginPage from '../../routes/LoginPage/LoginPage';
+import NewUserPage from '../../routes/NewUserPage/NewUserPage';
+import HomePage from '../../routes/HomePage/HomePage';
+import ExpandedMoviePage from '../../routes/ExpandedMoviePage/ExpandedMoviePage';
+import NewMoviePage from '../../routes/NewMoviePage/NewMoviePage';
+import MoviesContext from '../../contexts/MoviesContext';
+import config from '../config';
 import './App.css';
 
 class App extends Component {
+  state = {
+    movies: [],
+    error: null,
+  };
+
+  setMovies = movies => {
+    this.setState({
+      movies,
+      error: null,
+    })
+  }
+
+  addMovie = movie => {
+    this.setState({
+      movies: [ ...this.state.movies, movie ],
+    })
+  }
+
+  deleteMovie = movieId => {
+    const newMovies = this.state.movies.filter(movie =>
+      movie.id !== movieId
+      )
+      this.setState({
+        movies: newMovies
+      })
+  }
+
+  componentDidMount() {
+    fetch(config.API_ENDPOINT, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_KEY}`
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        return response.json().then(error => Promise.reject(error))
+      }
+      return response.json()
+    })
+    .then(this.setMovies)
+    .catch(error => {
+      console.error(error)
+      this.setState({ error })
+    })
+  }
+
+  updateMovie = updatedMovie => {
+    this.setState({
+      movies: this.state.movies.map(movie =>
+        (movie.id !== updatedMovie.id) ? movie : updatedMovie)
+    })
+  }
+
   render() {
+    const contextValue = {
+      movies: this.state.movies,
+      addMovie: this.addMovie,
+      deleteMovie: this.deleteMovie,
+      updatedMovie: this.updateMovie,
+    }
+    
     return (
       <div className="App">
-        <nav className="Nav_bar">
-          <NavBar />
-        </nav>
-        <main className="App_main">
-          <Switch>
-            <Route
-              exact
-              path={'/'}
-              component={LandingPage}
-              />
-            <Route
-              path={'/login'}
-              component={LoginPage}
-              />
-            <Route
-              path={'/register'}
-              component={NewUserPage}
-              />
-            <Route
-              path={'/home/:userId'}
-              component={HomePage}
-              />
-            <Route
-              path={'/home/:movieId'}
-              component={ExpandedMoviePage}
-              />
-            <Route
-              path={'/add'}
-              component={NewMoviePage}
-              />
-          </Switch>
-        </main>
+        <MoviesContext.Provider value={contextValue}>
+          <nav className="Nav_bar">
+            <NavBar />
+          </nav>
+          <main className="App_main">
+            <Switch>
+              <Route
+                exact
+                path={'/'}
+                component={LandingPage}
+                />
+              <Route
+                path={'/login'}
+                component={LoginPage}
+                />
+              <Route
+                path={'/register'}
+                component={NewUserPage}
+                />
+              <Route
+                path={'/home/:userId'}
+                component={HomePage}
+                />
+              <Route
+                path={'/home/:movieId'}
+                component={ExpandedMoviePage}
+                />
+              <Route
+                path={'/add'}
+                component={NewMoviePage}
+                />
+            </Switch>
+          </main>
+        </MoviesContext.Provider>
       </div>
     );
   }
--- src/Components/NewMovieForm/NewMovieForm.js
+++ src/Components/NewMovieForm/NewMovieForm.js
@@ -1,21 +1,85 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MoviesContext from '../../contexts/MoviesContext';
+import config from '../../../config';
 import { Link } from 'react-router-dom';
 import './NewMovieForm.css';
 
 export default class NewMovieForm extends Component {
+  
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,
+  };
+
+  static contextType = MoviesContext;
+
+  state = {
+    error: null,
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { title, comments, rating } = event.target
+    const movie = {
+      title: title.value,
+      comments: comments.value,
+      rating: Number(rating.value),
+    }
+    this.setState({ error: null })
+    fetch(config.API_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${config.API_KEY}`
+      }
+    })
+    .then(data => {
+      title.value = ''
+      comments.value = ''
+      rating.value = ''
+      this.context.addMovie(data)
+      this.props.history.push('/:userId')
+    })
+    .catch(error => {
+      console.error(error)
+      this.setState({ error })
+    })
+  }
+
   render() {
+    const { error } = this.state
     return (
-      <form id="newMovie-form">
+      <form 
+        id="newMovie-form"
+        onSubmit={this.handleSubmit}>
+        <div className='AddMovie_error' role='alert'>
+          {error && <p>{error.message}</p>}
+        </div>
         <div className="form-section">
-          <label for="movie-title">Movie title: </label>
-          <input type="text" name="movie-title" placeholder="" required></input>
+          <label 
+            htmlFor="title">
+              Movie title: 
+          </label>
+          <input 
+            type='text' 
+            name='title'
+            id='title' 
+            placeholder='' 
+            required>
+          </input>
         </div>
         <div class="form-section">
-          <label for="comments-section">Comments: </label>
-          <textarea name="comments-section" rows="10"></textarea>
+          <label htmlFor="comments">Comments: </label>
+          <textarea name="comments" rows="10"></textarea>
         </div>
         <div class="form-section">
-          <label for="rating">Movie rating: </label>
+          <label 
+            htmlFor="rating">
+              Movie rating: 
+          </label>
           <select type="number" name="rating">
             <option>1 star</option>
             <option>2 star</option>
