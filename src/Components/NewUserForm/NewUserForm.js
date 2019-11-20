f768150459586ee599120a5ffb4d8686a7b936d4
Evan Vogts <evanjvogts@gmail.com>
2019-11-19 20:23:07 -0600

    updated register form and page

--- src/Components/NewUserForm/NewUserForm.js
+++ src/Components/NewUserForm/NewUserForm.js
@@ -1,28 +1,93 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { Button, Input, Required } from '../Utility/Utility'
 
 export default class NewUserForm extends Component {
+  static defaultProps = {
+    onRegistrationSuccess: () => {}
+  }
+
+  state = {
+    error: null
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { first_name, last_name, email, user_name, password } = event.target
+    console.log('New user form submitted')
+    console.log({ first_name, last_name, email, user_name, password })
+    first_name.value = ''
+    last_name.value = ''
+    email.value = ''
+    user_name.value = ''
+    password.value = ''
+    this.props.onRegistrationSuccess()
+  }
+
   render() {
+    const { error } = this.state
     return (
-      <form className="NewUserForm">
-        <div>
-          <label htmlFor="first-name">First name</label>
-          <input placeholder='First Name' type="text" name='first-name' id='first-name' />
+      <form 
+        className="NewUserForm"
+        onSubmit={this.handleSubmit}
+      >
+
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
         </div>
-        <div>
-          <label htmlFor="last-name">Last name</label>
-          <input type="text" name='last-name' id='last-name' placeholder='Last Name' />
+
+        <div className='first_name'>
+          <label htmlFor='NewUserForm_first_name'>
+            First name <Required />
+          </label>
+          <Input
+            type="text" 
+            name='first-name' 
+            id='NewUserForm_first_name' 
+            required>
+          </Input>
         </div>
-        <div>
-          <label htmlFor="username">Email</label>
-          <input type="text" name='username' id='username' />
+
+        <div className='last_name'>
+          <label htmlFor="NewUserForm_last_name">
+            Last name <Required />
+          </label>
+          <Input
+            type="text" 
+            name='last-name' 
+            id='NewUserForm_last_name' 
+            required>
+          </Input>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name='password' id='password' />
+
+        <div className='email'>
+          <label htmlFor="NewUserForm_email">
+            Email <Required />
+          </label>
+          <Input
+            type="text" 
+            name='email' 
+            id='NewUserForm_email' 
+            required>
+          </Input>
         </div>
-        <button type='submit'>Submit</button>
+
+        <div className='password'>
+          <label htmlFor="NewUserForm_password">
+            Password <Required />
+          </label>
+          <Input
+            type="text" 
+            name='password' 
+            id='NewUserForm_password' 
+            required>
+          </Input>
+        </div>
+
+        <Button type='submit'>Submit</Button>
+
         <Link to='/'>Back</Link>
+
       </form>
     )
   }
--- src/Routes/NewUserPage/NewUserPage.js
+++ src/Routes/NewUserPage/NewUserPage.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import NewUserForm from '../../components/NewUserForm/NewUserForm'
 
 export default class NewUserPage extends Component {
+  static defaultProps = {
+    history: {
+      push: () => {},
+    },
+  }
+
+  handleRegistrationSucess = user => {
+    const { history } = this.props
+    history.push('/login')
+  }
+
   render() {
     return (
       <section className="NewUserPage">
         <h2>Sign Up!</h2>
-        <NewUserForm />
+        <NewUserForm 
+        onRegistrationSuccess={this.handleRegistrationSucess}
+        />
       </section>
     )
   }
