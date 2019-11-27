0cf14aad01b6d8be19ecebe2447be669bb436bcf
Evan Vogts <evanjvogts@gmail.com>
2019-11-27 10:07:21 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api`,
-  // API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'localhost:8000/api',
   TOKEN_KEY: 'watchdog-auth-token',
 }
