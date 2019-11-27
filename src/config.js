7716aa4a46445f82f63268568ba75ddf9a213981
Evan Vogts <evanjvogts@gmail.com>
2019-11-27 15:44:19 -0600

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
-  // API_ENDPOINT: `https://stark-waters-29564.herokuapp.com/api` || 'http://localhost:8000/api',
+  // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: `https://stark-waters-29564.herokuapp.com/api` || 'http://localhost:8000/api',
   TOKEN_KEY: 'watchdog-auth-token',
 }
